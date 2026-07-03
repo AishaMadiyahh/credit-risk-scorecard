255321e5ac7d7b057e5d12bcd718e7f2a803b732
AishaMadiyahh <madiyahaisha@gmail.com>
2026-07-04 00:32:58 +0200

    Diff Dataset: use german-credit-data

--- download_data.py
+++ download_data.py
@@ -1,9 +1,15 @@
-import requests
+# import requests
 
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data"
-response = requests.get(url)
+# url = "https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data"
+# response = requests.get(url)
 
-with open("german_credit.data", "wb") as file:
-    file.write(response.content)
+# with open("german_credit.data", "wb") as file:
+#     file.write(response.content)
 
-print("Download complete")
+# print("Download complete")
+
+import kagglehub
+
+path = kagglehub.dataset_download("kabure/german-credit-data-with-risk")
+
+print("Dataset downloaded to:", path)
