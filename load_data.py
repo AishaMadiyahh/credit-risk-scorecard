cd89e82c58ef94385549a3347cd33cafce676192
AishaMadiyahh <madiyahaisha@gmail.com>
2026-07-05 03:20:26 +0200

    Connect Python to SQL Server, load cleaned data into DataFrame

--- load_data.py
+++ load_data.py
@@ -6,12 +6,12 @@ connection_string = (
     "Server=AISHAMADIYAH\\SQLEXPRESS;"
     "Database=CreditRiskDB;"
     "Trusted_Connection=yes;"
-)
+) #contains the info that python needs to find & log into my SQL server
 
-connection = pyodbc.connect(connection_string)
+connection = pyodbc.connect(connection_string) #actually does the opeining of the connection using prior info
 
 query = "select * from credit_applications"
-df = pd.read_sql(query, connection)
+df = pd.read_sql(query, connection) #runs the query and stores the result in a pandas dataframe
 
 print(df.head())
 print("Shape:", df.shape)
